5dee975faa3897a65162f9c6047440684e04888b
Nate <set5@163.com>
2018-05-20 03:11:46 +0800

    添加HeroService服务

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES} from '../mock-heroes';
+// import { HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,15 +15,21 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
-  heroes = HEROES;
+  // heroes = HEROES;
+  heroes: Hero[];
   // selectedHero: Hero = HEROES[0];
   selectedHero: Hero;
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
