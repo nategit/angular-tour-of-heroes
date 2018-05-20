5d2ac93321744970556e23a139b931e6b3ad4545
Nate <set5@163.com>
2018-05-20 18:39:40 +0800

    添加MessageService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,20 +1,23 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
 import {HEROES} from './mock-heroes';
 import {Hero} from './hero';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) {
+  }
 
   // getHeroes(): Hero [] {
   //   return HEROES;
   // }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('heroService fetch heroes');
     return of(HEROES);
   }
 }
